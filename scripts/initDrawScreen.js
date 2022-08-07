1d0ea0ef844c52fd501e9c9d6019813dd8ca25b0
Melvin Wahlberg <mellepella@gmail.com>
2022-08-07 12:06:34 +0200

    Fix prevent default

--- scripts/initDrawScreen.js
+++ scripts/initDrawScreen.js
@@ -60,7 +60,6 @@ function initDrawScreen() {
   });
   canvas.addEventListener("mouseup", (ev) => penMachine.send("UP", ev));
   canvas.addEventListener("mousemove", (evt) => {
-    evt.preventDefault();
     const rect = evt.currentTarget.getBoundingClientRect();
     return penMachine.send("MOVE", {
       x: ((evt.clientX - rect.left) / (rect.right - rect.left)) * canvas.width,
@@ -70,7 +69,7 @@ function initDrawScreen() {
   canvas.addEventListener("mouseleave", (ev) => penMachine.send("EXIT", ev));
   canvas.addEventListener("touchstart", (ev) => penMachine.send("DOWN", ev));
   canvas.addEventListener("touchmove", (ev) => {
-    evt.preventDefault();
+    ev.preventDefault();
     const touch = ev.touches[0];
     const rect = ev.currentTarget.getBoundingClientRect();
     penMachine.send("MOVE", {
