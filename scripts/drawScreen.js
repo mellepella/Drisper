da03bd241ef849282ae2d2cbf2ee113f13517fc2
Melvin Wahlberg <mellepella@gmail.com>
2022-08-08 09:41:27 +0200

    Add undo button

--- scripts/drawScreen.js
+++ scripts/drawScreen.js
@@ -7,6 +7,7 @@ function machine(initState, definition) {
 
       if (didChange) {
         definition?.[this.state]?.onExit?.({ event, payload });
+        definition?.[nextState]?.onEnter?.({ event, payload });
       }
 
       if (nextState) {
@@ -18,60 +19,104 @@ function machine(initState, definition) {
   };
 }
 
+function mouseEventToCoord(evt) {
+  const canvas = evt.currentTarget;
+  const rect = evt.currentTarget.getBoundingClientRect();
+  return {
+    x: ((evt.clientX - rect.left) / (rect.right - rect.left)) * canvas.width,
+    y: ((evt.clientY - rect.top) / (rect.bottom - rect.top)) * canvas.height,
+  };
+}
+
+function touchEventToCoord(ev) {
+  const touch = ev.touches[0];
+  const rect = ev.currentTarget.getBoundingClientRect();
+  return {
+    x: ((touch.clientX - rect.left) / (rect.right - rect.left)) * canvas.width,
+    y: ((touch.clientY - rect.top) / (rect.bottom - rect.top)) * canvas.height,
+  };
+}
+
 function registerListeners(canvas, penMachine) {
   listener(canvas, "mousedown", (evt) => {
-    const rect = evt.currentTarget.getBoundingClientRect();
-    return penMachine.send("DOWN", {
-      x: ((evt.clientX - rect.left) / (rect.right - rect.left)) * canvas.width,
-      y: ((evt.clientY - rect.top) / (rect.bottom - rect.top)) * canvas.height,
-    });
+    return penMachine.send("DOWN", mouseEventToCoord(evt));
   });
-  listener(canvas, "mouseup", (ev) => penMachine.send("UP", ev));
+  listener(canvas, "mouseup", (ev) =>
+    penMachine.send("UP", mouseEventToCoord(ev))
+  );
   listener(canvas, "mousemove", (evt) => {
-    const rect = evt.currentTarget.getBoundingClientRect();
-    return penMachine.send("MOVE", {
-      x: ((evt.clientX - rect.left) / (rect.right - rect.left)) * canvas.width,
-      y: ((evt.clientY - rect.top) / (rect.bottom - rect.top)) * canvas.height,
-    });
+    return penMachine.send("MOVE", mouseEventToCoord(evt));
   });
-  listener(canvas, "mouseleave", (ev) => penMachine.send("EXIT", ev));
-  listener(canvas, "touchstart", (ev) => penMachine.send("DOWN", ev));
+  listener(canvas, "mouseleave", (ev) =>
+    penMachine.send("EXIT", mouseEventToCoord(ev))
+  );
+  listener(canvas, "touchstart", (ev) =>
+    penMachine.send("DOWN", touchEventToCoord(ev))
+  );
   listener(canvas, "touchmove", (ev) => {
     ev.preventDefault();
-    const touch = ev.touches[0];
-    const rect = ev.currentTarget.getBoundingClientRect();
-    penMachine.send("MOVE", {
-      x:
-        ((touch.clientX - rect.left) / (rect.right - rect.left)) * canvas.width,
-      y:
-        ((touch.clientY - rect.top) / (rect.bottom - rect.top)) * canvas.height,
-    });
+    penMachine.send("MOVE", touchEventToCoord(ev));
   });
-  listener(canvas, "touchend", (ev) => penMachine.send("UP", ev));
+  listener(canvas, "touchend", (ev) =>
+    penMachine.send("UP", touchEventToCoord(ev))
+  );
 }
 
 function getPenMachine(ctx, penSize) {
   let lastPosition;
+  let sequenceStarts = [];
+  let strokeStyle;
+  let moves = [];
 
-  return machine("IDLE", {
+  window.penMachine = machine("IDLE", {
+    UNDOING: {
+      on: {
+        EXIT: "IDLE",
+      },
+      onTouch: () => {
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+        const sequenceStart = Math.max(sequenceStarts.pop(), 0);
+        moves = moves.slice(0, sequenceStart);
+        moves.forEach((move) => {
+          const { startPosition, endPosition } = move;
+          ctx.beginPath();
+          ctx.lineCap = "round";
+          ctx.lineWidth = penSize;
+          ctx.strokeStyle = move.strokeStyle;
+          ctx.moveTo(startPosition.x, startPosition.y);
+          ctx.lineTo(endPosition.x, endPosition.y);
+          ctx.stroke();
+        });
+        window.penMachine.send("EXIT");
+      },
+    },
     DRAW: {
       onExit: () => {
         lastPosition = undefined;
       },
+      onEnter: () => {
+        sequenceStarts.push(moves.length);
+      },
       onTouch: ({ payload }) => {
         const startPosition = lastPosition ?? payload;
         const endPosition = payload;
+        strokeStyle = document.querySelector(
+          'input[name="color"]:checked'
+        ).value;
 
         ctx.beginPath();
         ctx.lineCap = "round";
         ctx.lineWidth = penSize;
-        ctx.strokeStyle = document.querySelector(
-          'input[name="color"]:checked'
-        ).value;
+        ctx.strokeStyle = strokeStyle;
         ctx.moveTo(startPosition.x, startPosition.y);
         ctx.lineTo(endPosition.x, endPosition.y);
         ctx.stroke();
         lastPosition = payload;
+        moves.push({
+          startPosition,
+          endPosition,
+          strokeStyle,
+        });
       },
       on: {
         UP: "IDLE",
@@ -79,6 +124,7 @@ function getPenMachine(ctx, penSize) {
         MOVE: "DRAW",
         EXIT: "IDLE",
         TIMES_UP: "DISABLED",
+        UNDO: "UNDOING",
       },
     },
     DISABLED: {},
@@ -89,9 +135,15 @@ function getPenMachine(ctx, penSize) {
         MOVE: "IDLE",
         EXIT: "IDLE",
         TIMES_UP: "DISABLED",
+        UNDO: "UNDOING",
       },
     },
   });
+  return window.penMachine;
+}
+
+function undo() {
+  window.penMachine.send("UNDO");
 }
 
 function initDrawScreen() {
@@ -109,7 +161,7 @@ function initDrawScreen() {
   const ctx = canvas.getContext("2d");
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   const penSize = canvas.clientWidth / 100;
-  const penMachine = getPenMachine(ctx, penSize);
+  penMachine = getPenMachine(ctx, penSize);
   registerListeners(canvas, penMachine);
 
   interval(() => {
